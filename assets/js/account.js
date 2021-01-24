53391737d031e17f1e27336a1d345fdcbbb9ff18
Asadbek <67479609+azoirov@users.noreply.github.com>
2021-01-24 15:51:11 +0500

    Revert "Not Fully"
    This reverts commit 14dbba81d307995fdc92fafaa27e81e101676f37.

--- assets/js/account.js
+++
@@ -1,8 +0,0 @@
-$('.hamburger').click(function() {
-    $('.body').hide(1000);
-    $('.responsive').fadeIn(3000);
-});
-$('.hamburger-close').click(function(){
-    $('.responsive').hide(1000);
-    $('.body').fadeIn(3000);
-});
